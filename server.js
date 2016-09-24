46d76cad62f87b30cf127ffa55a0495b0c0d564d
gopichanduppala <uppalagopichand@gmail.com>
2016-09-24 13:17:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,8 +105,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/ugc.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'ugc.png'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 

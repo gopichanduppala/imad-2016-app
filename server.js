af13b406367e0a25ffd2553f4c760291fddadf41
gopichanduppala <uppalagopichand@gmail.com>
2016-09-26 16:41:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,13 +54,13 @@ var articles = {
 };
   
 
-function createTemplate (data) {
+function createtemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
     
-    var htmlTemplate = `
+    var htmltemplate = `
     <html>
         <head>
            <title>

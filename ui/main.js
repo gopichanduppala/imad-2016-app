979e3be0b2e4041d3b38383d361548efd0cff510
gopichanduppala <uppalagopichand@gmail.com>
2016-09-26 17:07:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,1 @@
 console.log('Loaded!');
-
-// change the text of the main-text div
-var element=document.getElementById('main-text');
-
-element.innerHTML='new value';
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight () {
-    marginLeft=marginLeft=1;
-    img.style.marginLeft=marginLeft='px';
-}
-img.onclick = function () {
-    var interval=setInterval(moveRight,50);
-};

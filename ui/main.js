9783ba354f1e21b65fa792caef15fa6a440bd63a
gopichanduppala <uppalagopichand@gmail.com>
2016-09-26 14:42:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,10 @@ element.innerHTML='new value';
 
 //move the image
 var img = document.getElementById('madi');
-var marginleft=0;
+var marginLeft=0;
 function moveRight () {
-    marginLeft=marginleft=1;
-    img.style.marginLeft=marginleft='px';
+    marginLeft=marginLeft=1;
+    img.style.marginLeft=marginLeft='px';
 }
 img.onclick = function () {
     var interval=setInterval(moveRight,50);

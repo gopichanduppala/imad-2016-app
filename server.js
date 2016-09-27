569adbb12978515e38bd9449ee7f83558a5027c0
gopichanduppala <uppalagopichand@gmail.com>
2016-09-27 16:28:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,13 +94,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter =0;
+app.get('/counter',function(req,res){
+    counter =counter+1;
+    res.send(counter.tostring());
+});
+
 app.get('/:articlename',function(req,res){
     // articlename == Article-one
     // articles[articlename] == () content of article one
     var articlename = req.params.articlename;
     res.send(createTemplate(articles[articlename]));
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
